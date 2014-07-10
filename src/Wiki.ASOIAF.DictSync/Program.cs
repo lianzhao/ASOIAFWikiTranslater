4ae7b7fc8fd6950746d02e90a34790cbb16a596f
lianzhao <knife.fe@gmail.com>
2014-07-10 14:14:04 +0800

    code refine

--- src/Wiki.ASOIAF.DictSync/Program.cs
+++ src/Wiki.ASOIAF.DictSync/Program.cs
@@ -2,7 +2,6 @@
 {
     using System;
     using System.Collections.Generic;
-    using System.Globalization;
     using System.IO;
     using System.Linq;
     using System.Text.RegularExpressions;
@@ -21,59 +20,70 @@
         {
             try
             {
-                var wiki = new Wiki("Wiki.ASOIAF.DictSync", "zh.asoiaf.wikia.com", "/api.php");
-                var allPagesources =
-                    wiki.Query.allpages().Where(p => p.filterredir == allpagesfilterredir.nonredirects).Pages;
-                var results =
-                    allPagesources.Select(
-                        p => PageResult.Create(p.info, p.langlinks().Where(l => l.lang == "en").ToEnumerable()))
-                        .ToEnumerable();
-                var dict = results.Select(result => new { Lang = result.Data.FirstOrDefault(), Result = result })
-                    .Where(
-                        item =>
-                            {
-                                if (item.Lang == null || string.IsNullOrEmpty(item.Lang.value))
-                                {
-                                    Log.Warn(string.Format("Noen {0}", item.Result.Info.title));
-                                    return false;
-                                }
-
-                                if (!Regex.IsMatch(item.Lang.value.Substring(0, 1), "^[a-zA-Z]*$"))
-                                {
-                                    Log.Warn(
-                                        string.Format(
-                                            "Not en, title:{0}, lang:{1}",
-                                            item.Result.Info.title,
-                                            item.Lang.value));
-                                }
-
-                                return true;
-                            })
-                    .GroupBy(item => item.Lang.value)
-                    .Select(
-                        group =>
-                            {
-                                if (group.Count() > 1)
-                                {
-                                    Log.Warn(
-                                        string.Format(
-                                            "Duplicated item, en:{0}, ch:{1}",
-                                            group.Key,
-                                            string.Join(",", group.Select(item => item.Result.Info.title))));
-                                }
-
-                                return group.First();
-                            })
-                    .ToDictionary(item => item.Lang.value, item => item.Result.Info.title);
-                var json = JsonConvert.SerializeObject(dict);
-                using (var sw = new StreamWriter(@"..\..\..\..\dict\entry.json"))
-                {
-                    sw.Write(json);
-                }
+                var dict = GetDict();
+                PersistentDict(dict, @"..\..\..\..\dict\entry.json");
             }
             catch (Exception ex)
             {
                 Log.Fatal("Error occurred", ex);
+            }
+        }
+
+        private static IDictionary<string, string> GetDict()
+        {
+            var wiki = new Wiki("Wiki.ASOIAF.DictSync", "zh.asoiaf.wikia.com", "/api.php");
+            var allPagesources =
+                wiki.Query.allpages().Where(p => p.filterredir == allpagesfilterredir.nonredirects).Pages;
+            var results =
+                allPagesources.Select(
+                    p => PageResult.Create(p.info, p.langlinks().Where(l => l.lang == "en").ToEnumerable()))
+                    .ToEnumerable();
+            var dict = results.Select(result => new { Lang = result.Data.FirstOrDefault(), Result = result })
+                .Where(
+                    item =>
+                    {
+                        if (item.Lang == null || string.IsNullOrEmpty(item.Lang.value))
+                        {
+                            Log.Warn(string.Format("Noen {0}", item.Result.Info.title));
+                            return false;
+                        }
+
+                        if (!Regex.IsMatch(item.Lang.value.Substring(0, 1), "^[a-zA-Z]*$"))
+                        {
+                            Log.Warn(
+                                string.Format(
+                                    "Not en, title:{0}, lang:{1}",
+                                    item.Result.Info.title,
+                                    item.Lang.value));
+                        }
+
+                        return true;
+                    })
+                .GroupBy(item => item.Lang.value)
+                .Select(
+                    group =>
+                    {
+                        if (group.Count() > 1)
+                        {
+                            Log.Warn(
+                                string.Format(
+                                    "Duplicated item, en:{0}, ch:{1}",
+                                    group.Key,
+                                    string.Join(",", group.Select(item => item.Result.Info.title))));
+                        }
+
+                        return group.First();
+                    })
+                .ToDictionary(item => item.Lang.value, item => item.Result.Info.title);
+            return dict;
+        }
+
+        private static void PersistentDict(IDictionary<string, string> dict, string path)
+        {
+            var json = JsonConvert.SerializeObject(dict);
+            using (var sw = new StreamWriter(path))
+            {
+                sw.Write(json);
             }
         }
     }
